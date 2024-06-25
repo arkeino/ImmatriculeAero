41b0283702871b6a238d22876cb40308aedbb55a
KJoissaint <119964627+KJoissaint@users.noreply.github.com>
2024-06-25 14:14:59 +0200

    Update AeronefDTO.java

--- immataeronef/src/main/java/estiam/projets/immataeronef/AeronefDTO.java
+++ immataeronef/src/main/java/estiam/projets/immataeronef/AeronefDTO.java
@@ -13,34 +13,3 @@ public class AeronefDTO {
 	private String aerodromeAttache;
 
 }
-
-
-/*  package estiam.projets.immataeronef;
-public class AeronefDTO {
-
-    private String matricule;
-    private String constructeur;
-    private String modele;
-    private String aerodromeAttache;
-
-    public AeronefDTO(String matricule, String constructeur, String modele, String aerodromeAttache) {
-        this.matricule = matricule;
-        this.constructeur = constructeur;
-        this.modele = modele;
-        this.aerodromeAttache = aerodromeAttache;
-    }
-    public String getMatricule() {
-        return matricule;
-    }
-    public String getConstructeur() {
-        return constructeur;
-    }
-
-    public String getModele() {
-        return modele;
-    }
-
-    public String getAerodromeAttache() {
-        return aerodromeAttache;
-    }
-} */
